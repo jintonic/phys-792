707f2c91ab2128585ff19f3a2b0d4ebc80383211
Jing LIU <jing.liu@usd.edu>
2020-02-13 10:51:43 -0600

    added

---
+++ gaus.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <TRandom3.h>
+#include <TH1F.h>
+#include <TCanvas.h>
+int main ()
+{
+   TH1F histogram("histogram", ";Height [cm];",20, 100,300);
+   TRandom3 generator(0);
+
+   for (int i=0; i<160; i++) histogram.Fill(generator.Gaus(170,20));
+
+   TCanvas can;
+   histogram.Draw();
+   can.Print("height.jpg");
+}
